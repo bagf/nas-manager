60c03d107dc1051e01a80b1cccc3f33e700d9253
bagf <rory@eishgaming.co.za>
2016-09-25 22:21:35 +0200

    Moved directory scanning to a job

--- app/Console/Commands/AddItem.php
+++ app/Console/Commands/AddItem.php
@@ -4,10 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Bus\SelfHandling;
-use DirectoryIterator;
 use App\Item;
 use App\Category;
-use App\File;
+use App\Jobs\ScanItemFiles;
 
 class AddItem extends Command implements SelfHandling
 {
@@ -24,48 +23,6 @@ class AddItem extends Command implements SelfHandling
      * @var string
      */
     protected $description = '';
-
-    public function addFilePath($path)
-    {
-        $directory = new DirectoryIterator($path);
-        $dev = stat($path)[0];
-        $files = [];
-        $directories = [];
-        
-        foreach ($directory as $file) {
-            if ($file->isDot() || !$file->isReadable()) {
-                continue;
-            }
-            if ($file->isFile()) {
-                $node = $file->getInode();
-                if ($node <= 0 || $node == false) continue;
-                $f = File::where('dev', $dev)
-                    ->where('inode', $node)
-                    ->get();
-                if (is_null($f) || !count($f)) {
-                    $f = new File(['dev' => $dev, 'inode' => $node ]);
-                } else {
-                    $f = $f->first();
-                }
-                $f->filename = $file->getFilename();
-                $f->size = $file->getSize();
-                if ($f->isDirty()) {
-                    $f->save();
-                }
-                $files[] = $f->id;
-            } else if ($file->isDir()) {
-                $directories[] = $file->getPathname();
-            }
-        }
-        
-        foreach ($directories as $dir) {
-            $dirFiles = $this->addFilePath($dir);
-            $files = array_merge($files, $dirFiles);
-        }
-        
-        return $files;
-    }
-
 
     /**
      * Execute the console command.
@@ -96,8 +53,7 @@ class AddItem extends Command implements SelfHandling
         
         $item->save();
         
-        $files = $this->addFilePath($path);
-        $item->files()->sync($files);
+        dispatch(new ScanItemFiles($item, $path));
     }
     
     protected function category($name)
---
+++ app/Jobs/ScanItemFiles.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+use App\Item;
+use App\File;
+use DirectoryIterator;
+
+class ScanItemFiles extends Job implements SelfHandling
+{
+    protected $item;
+    protected $path;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Item $item, $path)
+    {
+        $this->item = $item;
+        $this->path = $path;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $files = $this->addFilePath($this->path);
+        $this->item->files()->sync($files);
+    }
+    
+    protected function addFilePath($path)
+    {
+        $directory = new DirectoryIterator($path);
+        $dev = stat($path)[0];
+        $files = [];
+        $directories = [];
+        
+        foreach ($directory as $file) {
+            if ($file->isDot() || !$file->isReadable()) {
+                continue;
+            }
+            if ($file->isFile()) {
+                $node = $file->getInode();
+                if ($node <= 0 || $node == false) continue;
+                $f = File::where('dev', $dev)
+                    ->where('inode', $node)
+                    ->get();
+                if (is_null($f) || !count($f)) {
+                    $f = new File(['dev' => $dev, 'inode' => $node ]);
+                } else {
+                    $f = $f->first();
+                }
+                $f->filename = $file->getFilename();
+                $f->size = $file->getSize();
+                if ($f->isDirty()) {
+                    $f->save();
+                }
+                $files[] = $f->id;
+            } else if ($file->isDir()) {
+                $directories[] = $file->getPathname();
+            }
+        }
+        
+        foreach ($directories as $dir) {
+            $dirFiles = $this->addFilePath($dir);
+            $files = array_merge($files, $dirFiles);
+        }
+        
+        return $files;
+    }
+}
